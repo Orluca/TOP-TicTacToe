45cd26600942e05b69f4290b96d64c297fccf73b
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-27 15:31:05 +0200

    Added function to display gameboard cells

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 "use strict";
 
 const Gameboard = (function () {
-  const gameboardArray = [1, 2, 3];
+  const gameboardArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return { gameboardArray };
 })();
 
 const DisplayController = (function () {
-  return;
+  const displayGameboard = function (array) {
+    const $gameboard = document.querySelector(".gameboard");
+    array.forEach((val) => {
+      $gameboard.insertAdjacentHTML("beforeend", `<div class="gameboard-cell">${val}</div>`);
+    });
+  };
+
+  return { displayGameboard };
 })();
 
 const Player = function (playerName) {
   const name = playerName;
   return { name };
 };
+
+DisplayController.displayGameboard(Gameboard.gameboardArray);
