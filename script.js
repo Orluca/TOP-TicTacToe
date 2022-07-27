a34a8a9c2d208529e837647195926d85d9f9971b
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-27 20:04:49 +0200

    Implemented first win logic

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 const Gameboard = (function () {
   const values = new Array(9).fill("");
 
-  function display(element) {
-    values.forEach((val, i) => element.insertAdjacentHTML("beforeend", `<div class="gameboard-cell" data-id="${i}">${val}</div>`));
+  function display(target) {
+    values.forEach((val, i) => target.insertAdjacentHTML("beforeend", `<div class="gameboard-cell" data-id="${i}">${val}</div>`));
   }
 
   function setCell(symbol, id) {
@@ -15,7 +15,16 @@ const Gameboard = (function () {
     return values[id];
   }
 
-  return { display, getCell, setCell };
+  function checkRows() {
+    // const row1 = new Set(values.slice(0, 3));
+    // const row2 = new Set(values.slice(3, 6));
+    // const row3 = new Set(values.slice(6, 9));
+    // console.log(row1);
+    // console.log(row2);
+    // console.log(row3);
+  }
+
+  return { display, getCell, setCell, checkRows };
 })();
 
 const Player = function (playerName, playerSymbol) {
@@ -34,25 +43,59 @@ const Game = (function () {
     activePlayer = activePlayer === playerUser ? playerComputer : playerUser;
   }
 
-  function getActiveSymbol() {
-    return activePlayer.symbol;
-  }
+  const winTracker = { row1: 0, row2: 0, row3: 0, col1: 0, col2: 0, col3: 0, diag1: 0, diag2: 0 };
 
-  function checkForWinner() {
-    if (Gameboard.getCell(0) === Gameboard.getCell(1) && Gameboard.getCell(0) === Gameboard.getCell(2) && Gameboard.getCell(1) === Gameboard.getCell(2) && Gameboard.getCell(0)) console.log("WIN");
+  function checkForWinner(id, symbol) {
+    const add = symbol === playerUser.symbol ? 1 : -1;
+    if (id === 0) {
+      winTracker.row1 += add;
+      winTracker.col1 += add;
+      winTracker.diag1 += add;
+    } else if (id === 1) {
+      winTracker.row1 += add;
+      winTracker.col2 += add;
+    } else if (id === 2) {
+      winTracker.row1 += add;
+      winTracker.col3 += add;
+      winTracker.diag2 += add;
+    } else if (id === 3) {
+      winTracker.row2 += add;
+      winTracker.col1 += add;
+    } else if (id === 4) {
+      winTracker.row2 += add;
+      winTracker.col2 += add;
+      winTracker.diag1 += add;
+      winTracker.diag2 += add;
+    } else if (id === 5) {
+      winTracker.row2 += add;
+      winTracker.col3 += add;
+    } else if (id === 6) {
+      winTracker.row3 += add;
+      winTracker.col1 += add;
+      winTracker.diag2 += add;
+    } else if (id === 7) {
+      winTracker.row3 += add;
+      winTracker.col2 += add;
+    } else if (id === 8) {
+      winTracker.row3 += add;
+      winTracker.col3 += add;
+      winTracker.diag1 += add;
+    }
+
+    return Object.values(winTracker).some((val) => val === 3 || val === -3);
   }
 
   function playRound(e) {
     const clickedCell = e.target;
     const symbol = activePlayer.symbol;
-    const cellId = clickedCell.dataset.id;
+    const cellId = Number(clickedCell.dataset.id);
 
     if (clickedCell.textContent) return; // Don't overwrite cells that have already been clicked
 
     clickedCell.textContent = symbol;
     Gameboard.setCell(symbol, cellId);
 
-    checkForWinner();
+    checkForWinner(cellId, symbol);
     switchActivePlayer();
   }
 
@@ -61,7 +104,7 @@ const Game = (function () {
     $gameboard.addEventListener("click", playRound);
   }
 
-  return { start, switchActivePlayer, getActiveSymbol, checkForWinner };
+  return { start };
 })();
 
 Game.start();
