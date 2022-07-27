230c0dfb46374d7a3d7d570291bce97fe11d1ddc
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-27 15:44:21 +0200

    Small Gameboard refactor

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const Gameboard = (function () {
+  const $gameboard = document.querySelector(".gameboard");
   const values = ["X", "O", "X", "O", "X", "O", "O", "X", "X"];
   const display = function () {
-    const $gameboard = document.querySelector(".gameboard");
     values.forEach((val) => {
       $gameboard.insertAdjacentHTML("beforeend", `<div class="gameboard-cell">${val}</div>`);
     });
