0ac11c0991f2306bb9dfce709d0133a778c17749
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-27 17:33:12 +0200

    Added basic drawing and player switching logic

--- script.js
+++ script.js
@@ -2,22 +2,46 @@
 
 const Gameboard = (function () {
   const $gameboard = document.querySelector(".gameboard");
-  const values = ["X", "O", "X", "O", "X", "O", "O", "X", "X"];
-  const display = function () {
-    values.forEach((val) => {
-      $gameboard.insertAdjacentHTML("beforeend", `<div class="gameboard-cell">${val}</div>`);
-    });
-  };
+  const values = new Array(9).fill("");
+
+  function display() {
+    values.forEach((val) => $gameboard.insertAdjacentHTML("beforeend", `<div class="gameboard-cell">${val}</div>`));
+  }
+
+  function draw(e) {
+    if (e.target.textContent) return;
+    e.target.textContent = Game.getSymbol();
+    Game.switchActivePlayer();
+  }
+
+  $gameboard.addEventListener("click", draw);
+
   return { display };
 })();
 
-const DisplayController = (function () {
-  return;
-})();
-
-const Player = function (playerName) {
-  const name = playerName;
-  return { name };
+const Player = function (playerSymbol) {
+  const symbol = playerSymbol;
+  return { symbol };
 };
 
-Gameboard.display();
+const Game = (function () {
+  const playerUser = Player("X");
+  const playerComputer = Player("O");
+  let activePlayer = playerUser;
+
+  function switchActivePlayer() {
+    activePlayer = activePlayer === playerUser ? playerComputer : playerUser;
+  }
+
+  function getSymbol() {
+    return activePlayer.symbol;
+  }
+
+  function start() {
+    Gameboard.display();
+  }
+
+  return { start, activePlayer, switchActivePlayer, getSymbol };
+})();
+
+Game.start();
