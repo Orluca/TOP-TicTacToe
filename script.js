cd830e1bb0ee4052a4615ef0d098598d4ebd79f5
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-27 15:25:09 +0200

    Basic gameboard added

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const Gameboard = (function () {
+  const gameboardArray = [1, 2, 3];
+
+  return { gameboardArray };
+})();
+
+const DisplayController = (function () {
+  return;
+})();
+
+const Player = function (playerName) {
+  const name = playerName;
+  return { name };
+};
