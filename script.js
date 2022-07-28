8f4cd78a2cad01bc05ecf19a0f87196bc5d91e5a
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-28 17:05:56 +0200

    Finished refactor

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const Player = function (name, symbol) {
 const Interface = (function () {
   const $gameboard = document.querySelector(".gameboard");
   const $btnStart = document.querySelector(".btn-start-game");
+  const $resultDisplay = document.querySelector(".result-display");
 
   function updateGameboard() {
     const values = Game.getGameboardValues();
@@ -16,21 +17,51 @@ const Interface = (function () {
   function handleCellClicks(e) {
     const clickedCell = e.target;
     const cellId = Number(clickedCell.dataset.id);
-    const symbol = Game.getActiveSymbol();
+    const symbol = Game.getActivePlayer().symbol;
 
     if (clickedCell.textContent) return; // Don't overwrite cells that have already been clicked
 
     drawSymbol(clickedCell, symbol);
     Game.setCell(cellId);
+
+    if (Game.checkForWinner(cellId)) {
+      disableInput();
+      showResult(Game.getActivePlayer().name);
+      return;
+    }
+    if (Game.checkIfFull()) {
+      disableInput();
+      showResult();
+      return;
+    }
     Game.switchActivePlayer();
+  }
+
+  function showResult(winner) {
+    $resultDisplay.classList.remove("hidden");
+
+    if (!winner) $resultDisplay.textContent = "It's a draw!";
+    else $resultDisplay.textContent = `${winner} wins!`;
+  }
+
+  function hideResult() {
+    $resultDisplay.classList.add("hidden");
   }
 
   function enableInput() {
     $gameboard.addEventListener("click", handleCellClicks);
   }
 
+  function disableInput() {
+    $gameboard.removeEventListener("click", handleCellClicks);
+  }
+
   function drawSymbol(clickedCell, symbol) {
     clickedCell.textContent = symbol;
+  }
+
+  function clearGameboard() {
+    $gameboard.innerHTML = "";
   }
 
   function init() {
@@ -38,22 +69,22 @@ const Interface = (function () {
     updateGameboard();
   }
 
-  return { init, enableInput };
+  return { init, enableInput, hideResult, clearGameboard, updateGameboard };
 })();
 
 const Game = (function () {
   const playerUser = Player("User", "X");
   const playerComputer = Player("CPU", "O");
   let activePlayer;
-
+  let winTracker;
   let values = new Array(9).fill("");
 
   function getGameboardValues() {
     return values;
   }
 
-  function getActiveSymbol() {
-    return activePlayer.symbol;
+  function getActivePlayer() {
+    return activePlayer;
   }
 
   function setCell(id) {
@@ -64,20 +95,69 @@ const Game = (function () {
     activePlayer = activePlayer === playerUser ? playerComputer : playerUser;
   }
 
-  function start() {
-    Interface.enableInput();
-    activePlayer = playerUser;
+  function checkForWinner(id) {
+    const add = activePlayer === playerUser ? 1 : -1;
+    if (id === 0) {
+      winTracker.row1 += add;
+      winTracker.col1 += add;
+      winTracker.diag1 += add;
+    } else if (id === 1) {
+      winTracker.row1 += add;
+      winTracker.col2 += add;
+    } else if (id === 2) {
+      winTracker.row1 += add;
+      winTracker.col3 += add;
+      winTracker.diag2 += add;
+    } else if (id === 3) {
+      winTracker.row2 += add;
+      winTracker.col1 += add;
+    } else if (id === 4) {
+      winTracker.row2 += add;
+      winTracker.col2 += add;
+      winTracker.diag1 += add;
+      winTracker.diag2 += add;
+    } else if (id === 5) {
+      winTracker.row2 += add;
+      winTracker.col3 += add;
+    } else if (id === 6) {
+      winTracker.row3 += add;
+      winTracker.col1 += add;
+      winTracker.diag2 += add;
+    } else if (id === 7) {
+      winTracker.row3 += add;
+      winTracker.col2 += add;
+    } else if (id === 8) {
+      winTracker.row3 += add;
+      winTracker.col3 += add;
+      winTracker.diag1 += add;
+    }
+    return Object.values(winTracker).some((val) => val === 3 || val === -3);
   }
 
-  function stop() {
-    console.log("STOPP");
+  function checkIfFull() {
+    if (!values.includes("")) return true;
+  }
+
+  function resetVariables() {
+    activePlayer = playerUser;
+    winTracker = { row1: 0, row2: 0, row3: 0, col1: 0, col2: 0, col3: 0, diag1: 0, diag2: 0 };
+    values = new Array(9).fill("");
+  }
+
+  function start() {
+    resetVariables();
+
+    Interface.enableInput();
+    Interface.hideResult();
+    Interface.clearGameboard();
+    Interface.updateGameboard();
   }
 
   function init() {
     Interface.init();
   }
 
-  return { init, getGameboardValues, getActiveSymbol, setCell, start, switchActivePlayer };
+  return { init, getGameboardValues, getActivePlayer, setCell, start, switchActivePlayer, checkForWinner, checkIfFull };
 })();
 
 Game.init();
