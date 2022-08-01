480b5eb9aa5b2f7fffeee87d75e0632973c7f325
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-08-01 13:43:19 +0200

    minimax setup

--- script.js
+++ script.js
@@ -25,20 +25,56 @@ function userMove(id) {
   updateGameboard();
 }
 
-function computerMoveRandom() {
+function cellIsOccupied(id) {
+  return gameboard[id] ? true : false;
+}
+
+function getRandomFreeCell() {
   const freeCells = gameboard.reduce((acc, cell, i) => {
     if (cell === "") acc.push(i);
     return acc;
   }, []);
-  console.log(freeCells);
-  const randomID = Math.floor(Math.random() * freeCells.length);
-  gameboard[freeCells[randomID]] = "O";
+  return freeCells[Math.floor(Math.random() * freeCells.length)];
+}
+
+function computerMoveEasy() {
+  gameboard[getRandomFreeCell()] = "O";
   updateGameboard();
 }
 
+function computerMoveImpossible() {
+  // Go through each remaining empty cell and calc its score with minimax. Then choose the cell with highest score and place "O"
+  let bestScore = -Infinity;
+  let bestMove;
+
+  gameboard.forEach((cell, i) => {
+    if (cell === "") {
+      // Calc the score for the current cell using MINIMAX
+      const score = minimax();
+      // If the score is bigger than the current bestScore, reassign it and use current move as best move
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = i;
+      }
+    }
+  });
+
+  gameboard[bestMove] = "O";
+  updateGameboard();
+}
+
+function minimax() {
+  console.log("minimax");
+  return 1;
+}
+
 function handleCellClicks(e) {
+  const id = e.target.dataset.id;
+  if (cellIsOccupied(id)) return;
+
   userMove(e.target.dataset.id);
-  computerMoveRandom();
+  //   computerMoveEasy();
+  computerMoveImpossible();
 }
 
 // ###############################################################
