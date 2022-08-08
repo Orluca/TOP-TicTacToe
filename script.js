0503a94429c6f9b793467d2eb0faa8d86d2b81a6
Orluca <67704819+Orluca@users.noreply.github.com>
2022-08-08 12:45:34 +0200

    Start game with Enter press

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ const Interface = (function () {
   const $cells = document.querySelectorAll(".cell");
   const $scoreP1 = document.querySelector("#score-value-player1");
   const $scoreP2 = document.querySelector("#score-value-player2");
+  // const $nameInputsContainer = document.querySelector(".name-inputs");
   const $nameInputP1 = document.querySelector("#input-name-p1");
   const $nameInputP2 = document.querySelector("#input-name-p2");
   const $nameDisplayP1 = document.querySelector("#name-display-p1");
@@ -38,6 +39,12 @@ const Interface = (function () {
     $opponentSelectionSection.addEventListener("click", handleOpponentSelection);
     $difficultySelectionSection.addEventListener("click", handleDifficultySelection);
     $gameboard.addEventListener("click", handleGameboardClicks);
+    $nameInputsContainer.addEventListener("keypress", listenForEnterPresses);
+  }
+
+  function listenForEnterPresses(e) {
+    if (e.key !== "Enter") return;
+    handleStartGamePresses();
   }
 
   function handleGameboardClicks(e) {
